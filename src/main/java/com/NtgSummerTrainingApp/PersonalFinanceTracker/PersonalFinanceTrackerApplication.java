00e3866f9057585636d5c153b295377da9287e4d
Shaaamy <amrshamy91@gmail.com>
2025-08-16 17:46:01 +0300

    feat: initialize Spring Boot project with PostgreSQL configuration

---
+++ src/main/java/com/NtgSummerTrainingApp/PersonalFinanceTracker/PersonalFinanceTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.NtgSummerTrainingApp.PersonalFinanceTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Personal Finance Tracker App");
+		SpringApplication.run(PersonalFinanceTrackerApplication.class, args);
+	}
+
+}
