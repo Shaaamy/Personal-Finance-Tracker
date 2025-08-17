2de0bfbe295d38aea0a651d1b094616ba43ee1f1
Amr Shamy <104179365+Shaaamy@users.noreply.github.com>
2025-08-17 16:43:15 +0300

    feat: Add create , findById , findAll endpoints

---
+++ src/main/java/com/NtgSummerTrainingApp/PersonalFinanceTracker/Controllers/CategoryController.java
@@ -0,0 +1,34 @@
+package com.NtgSummerTrainingApp.PersonalFinanceTracker.Controllers;
+
+import com.NtgSummerTrainingApp.PersonalFinanceTracker.Services.CategoryService;
+import com.NtgSummerTrainingApp.PersonalFinanceTracker.dto.CategoryRequestDto;
+import com.NtgSummerTrainingApp.PersonalFinanceTracker.dto.CategoryResponseDto;
+import jakarta.validation.Valid;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Data
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createCategory(@RequestBody @Valid CategoryRequestDto categoryReq){
+        return new ResponseEntity<>( categoryService.createNewCategory(categoryReq), HttpStatus.CREATED);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponseDto> getCategoryById(@PathVariable long id){
+        return new ResponseEntity<>(categoryService.findCategoryById(id),HttpStatus.OK);
+    }
+    @GetMapping()
+    public ResponseEntity<List<CategoryResponseDto>> getAllCategories(){
+        return new ResponseEntity<>(categoryService.findAllCategories(),HttpStatus.OK);
+    }
+
+
+}
