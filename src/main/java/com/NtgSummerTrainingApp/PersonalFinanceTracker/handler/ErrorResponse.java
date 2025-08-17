3ad6262df0734c1cee42ae530013c3611ad676d9
Amr Shamy <104179365+Shaaamy@users.noreply.github.com>
2025-08-17 16:55:15 +0300

    feat: create GlobalExceptionHandler and ErrorResponse

---
+++ src/main/java/com/NtgSummerTrainingApp/PersonalFinanceTracker/handler/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.NtgSummerTrainingApp.PersonalFinanceTracker.handler;
+
+import lombok.Data;
+
+@Data
+public class ErrorResponse {
+    private String message;
+    private String code;
+
+    public ErrorResponse(String message, String code) {
+        this.message = message;
+        this.code = code;
+    }
+}
