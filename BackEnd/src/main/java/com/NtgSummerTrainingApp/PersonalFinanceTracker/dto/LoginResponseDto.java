bb02603b433f4dedb7856d6c55f7548946d1f53d
Shaaamy <amrshamy91@gmail.com>
2025-08-23 02:53:15 +0300

    feat: Add jwt configurations

---
+++ BackEnd/src/main/java/com/NtgSummerTrainingApp/PersonalFinanceTracker/dto/LoginResponseDto.java
@@ -0,0 +1,18 @@
+package com.NtgSummerTrainingApp.PersonalFinanceTracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class LoginResponseDto {
+    private String token;
+    private String username;
+    private String fullName;
+    private String role;
+}
