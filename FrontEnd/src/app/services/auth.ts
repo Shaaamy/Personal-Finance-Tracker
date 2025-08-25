767f4ef9182bd5701f4de704b18c7843d40ff707
PC <abdosaeed079@gmail.com>
2025-08-25 22:11:57 +0300

    integration

---
+++ FrontEnd/src/app/services/auth.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Auth {
+
+  constructor(private http: HttpClient) {}
+
+  register(user: any): Observable<any> {
+    return this.http.post<any>('http://localhost:8081/user/register', user);
+  }
+
+}
--- FrontEnd/src/app/signup/signup.ts
+++ FrontEnd/src/app/signup/signup.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Auth } from '../services/auth';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.html',
-  styleUrls: ['./signup.css']
+  styleUrls: ['./signup.css'],
+  imports: [ReactiveFormsModule]
 })
 export class Signup {
   logoUrl: string = 'https://agency.entasher.com/Images/actualsize/2024/5/Logo_847363b7554a46b48559c222f4f9053c.png';
 
+  signupForm = new FormGroup({
+    username: new FormControl(''),
+    email: new FormControl(''),
+    fullName: new FormControl(''),
+    password: new FormControl(''),
+    role: new FormControl(''),
+    balance: new FormControl('')
+  });
   onSubmit() {
-    alert('Account created successfully (demo)');
+    console.log(this.signupForm.value);
+    this.authService.register(this.signupForm.value).subscribe((res) => {
+      console.log(res);
+    });
   }
-   constructor(private router: Router) {}
+   constructor(private router: Router, private authService: Auth) {}
 
   goTo() {
     this.router.navigate(['/login']);
