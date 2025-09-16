ae9973496930e023d946ce964299fe0b1f2ab4f2
202201638 <s-ahmed.sharaf@zewailcity.edu.eg>
2025-09-16 09:29:30 +0300

    a

--- FrontEnd/src/app/signup/signup.ts
+++ FrontEnd/src/app/signup/signup.ts
@@ -1,38 +1,43 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Auth } from '../services/auth';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';   // ⬅️ استيراد CommonModule
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.html',
   styleUrls: ['./signup.css'],
-  imports: [ReactiveFormsModule]
+  standalone: true,                               // (تأكد أنها موجودة)
+  imports: [ReactiveFormsModule, CommonModule]    // ⬅️ أضف CommonModule هنا
 })
 export class Signup {
   logoUrl: string = 'https://agency.entasher.com/Images/actualsize/2024/5/Logo_847363b7554a46b48559c222f4f9053c.png';
 
   signupForm = new FormGroup({
-    username: new FormControl(''),
-    email: new FormControl(''),
-    fullName: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    fullName: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required),
     role: new FormControl(''),
-    balance: new FormControl('')
+    balance: new FormControl('', Validators.required)
   });
+
+  constructor(private router: Router, private authService: Auth) {}
+
   onSubmit() {
+    if (this.signupForm.invalid) {
+      this.signupForm.markAllAsTouched();
+      return;
+    }
     console.log(this.signupForm.value);
-    this.authService.register(this.signupForm.value).subscribe((res) => {
+    this.authService.register(this.signupForm.value).subscribe(res => {
       console.log(res);
+      this.router.navigate(['/home']);
     });
   }
-   constructor(private router: Router, private authService: Auth) {}
 
-goToSignup() {
-  this.router.navigate(['/signup']);  
-}
-  goTo() {
-    this.router.navigate(['/login']);
-  }
+  goToSignup() { this.router.navigate(['/home']); }
+  goTo() { this.router.navigate(['/login']); }
 }
