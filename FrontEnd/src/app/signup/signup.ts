6549a745022f0036136fcdc10bb91d7d37fe391d
202201638 <s-ahmed.sharaf@zewailcity.edu.eg>
2025-09-16 17:31:59 +0300

    last

--- FrontEnd/src/app/signup/signup.ts
+++ FrontEnd/src/app/signup/signup.ts
@@ -2,21 +2,22 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Auth } from '../services/auth';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';   // ⬅️ استيراد CommonModule
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.html',
   styleUrls: ['./signup.css'],
-  standalone: true,                               // (تأكد أنها موجودة)
-  imports: [ReactiveFormsModule, CommonModule]    // ⬅️ أضف CommonModule هنا
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule]
 })
 export class Signup {
-  logoUrl: string = 'https://agency.entasher.com/Images/actualsize/2024/5/Logo_847363b7554a46b48559c222f4f9053c.png';
+  logoUrl: string =
+    'https://agency.entasher.com/Images/actualsize/2024/5/Logo_847363b7554a46b48559c222f4f9053c.png';
 
   signupForm = new FormGroup({
     username: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
     fullName: new FormControl('', Validators.required),
     password: new FormControl('', Validators.required),
     confirmPassword: new FormControl('', Validators.required),
@@ -28,9 +29,11 @@ export class Signup {
 
   onSubmit() {
     if (this.signupForm.invalid) {
+      // إظهار رسائل الخطأ لكل الحقول
       this.signupForm.markAllAsTouched();
       return;
     }
+
     console.log(this.signupForm.value);
     this.authService.register(this.signupForm.value).subscribe(res => {
       console.log(res);
@@ -38,6 +41,10 @@ export class Signup {
     });
   }
 
-  goToSignup() { this.router.navigate(['/home']); }
-  goTo() { this.router.navigate(['/login']); }
-}
+  goToSignup() {
+    this.router.navigate(['/home']);
+  }
+  goTo() {
+    this.router.navigate(['/login']);
+  }
+}   
